4a1c89c12de7681a422f115d9066a07fdbf81baf
brucehan15 <77654243+brucehan15@users.noreply.github.com>
2021-01-19 09:29:37 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,16 +2,17 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(` Logged in as ${client.user.tag}!` );
     client.user.setActivity('spring snow by the boyz', { type: 'WATCHING' });
 });
 
 
 client.on('message', message => {
     const args = message.content.split(" ").slice(1);
-    if(message.content.startsWith('r!say') && message.guild.member(message.author).hasPermission("MANAGE_CHANNELS")) {
+    if(message.content.startsWith('lj!say') && message.guild.member(message.author).hasPermission("MANAGE_CHANNELS")) {
         message.delete()
         var saytext = args.join(" ");
         message.channel.send(saytext);
     }
   });
+client.login(process.env.BOT_TOKEN);
